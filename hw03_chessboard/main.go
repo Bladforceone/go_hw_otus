03c815e3e7cdcbb30a754336148896289041ca69
Vladislav Kuprin <bladforceone@gmail.com>
2024-10-17 10:14:50 +0300

    HW03 is completed

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,44 @@
 package main
 
+import (
+	"fmt"
+)
+
+func printChessboard(size int) error {
+	var evenRow, oddRow string
+
+	if size < 2 {
+		return fmt.Errorf("uncorrected size")
+	}
+
+	for i := 0; i < size; i++ {
+		if i%2 == 0 {
+			evenRow += " "
+			oddRow += "#"
+		} else {
+			evenRow += "#"
+			oddRow += " "
+		}
+	}
+
+	for i := 0; i < size; i++ {
+		if i%2 == 0 {
+			fmt.Printf("%s \n", evenRow)
+		} else {
+			fmt.Printf("%s \n", oddRow)
+		}
+	}
+
+	return nil
+}
+
 func main() {
-	// Place your code here.
+	var size int
+
+	fmt.Scan(&size)
+
+	err := printChessboard(size)
+	if err != nil {
+		fmt.Print(err)
+	}
 }
