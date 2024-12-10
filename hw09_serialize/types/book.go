fac9cc7644d59d12fd2860716a18fa00ee97dff9
Vladislav Kuprin <bladforceone@gmail.com>
2024-12-10 12:02:49 +0300

    Task is completed

--- hw09_serialize/types/book.go
+++ hw09_serialize/types/book.go
@@ -1,52 +1,77 @@
 package types
 
-import "encoding/json"
+import (
+	"bytes"
+	"encoding/gob"
+	"encoding/json"
+	"encoding/xml"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"gopkg.in/yaml.v3"
+)
 
 type Book struct {
-	ID     int
-	Title  string
-	Author string
-	Year   int
-	Size   int
-	Rate   float64
+	ID     int     `json:"id" xml:"id" yaml:"id"`
+	Title  string  `json:"title" xml:"title" yaml:"title"`
+	Author string  `json:"author" xml:"author" yaml:"author"`
+	Year   int     `json:"year" xml:"year" yaml:"year"`
+	Size   int     `json:"size" xml:"size" yaml:"size"`
+	Rate   float32 `json:"rate" xml:"rate" yaml:"rate"`
+	Sample []byte  `json:"sample" xml:"sample" yaml:"sample"`
 }
 
-func (b *Book) MarshalJSON() ([]byte, error) {
-	return json.Marshal(struct {
-		ID     int     `json:"id"`
-		Title  string  `json:"title"`
-		Author string  `json:"author"`
-		Year   int     `json:"year"`
-		Size   int     `json:"size"`
-		Rate   float64 `json:"rate"`
-	}{
-		ID:     b.ID,
-		Title:  b.Title,
-		Author: b.Author,
-		Year:   b.Year,
-		Size:   b.Size,
-		Rate:   b.Rate,
-	})
+func UnmarshalJSONSlices(data []byte) ([]Book, error) {
+	var tmp []Book
+	return tmp, json.Unmarshal(data, &tmp)
 }
 
-func (b *Book) UnmarshalJSON(data []byte) error {
-	var aux struct {
-		ID     int     `json:"id"`
-		Title  string  `json:"title"`
-		Author string  `json:"author"`
-		Year   int     `json:"year"`
-		Size   int     `json:"size"`
-		Rate   float64 `json:"rate"`
-	}
-	err := json.Unmarshal(data, &aux)
-	if err != nil {
+func MarshalJSONSlices(books []Book) ([]byte, error) {
+	return json.Marshal(books)
+}
+
+func UnmarshalXMLSlices(data []byte) ([]Book, error) {
+	var tmp []Book
+	return tmp, xml.Unmarshal(data, &tmp)
+}
+
+func MarshalXMLSlices(books []Book) ([]byte, error) {
+	return xml.Marshal(books)
+}
+
+func UnmarshalYAMLSlices(data []byte) ([]Book, error) {
+	var tmp []Book
+	return tmp, yaml.Unmarshal(data, &tmp)
+}
+
+func MarshalYAMLSlices(books Book) ([]byte, error) {
+	return yaml.Marshal(books)
+}
+
+func (b *Book) UnmarshalGob(data []byte) error {
+	dec := gob.NewDecoder(bytes.NewReader(data))
+	if err := dec.Decode(b); err != nil {
 		return err
 	}
-	b.ID = aux.ID
-	b.Title = aux.Title
-	b.Author = aux.Author
-	b.Year = aux.Year
-	b.Size = aux.Size
-	b.Rate = aux.Rate
 	return nil
+}
+
+func (b *Book) MarshalGob() ([]byte, error) {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	if err := enc.Encode(b); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
+}
+
+func (b *Book) UnmarshalBSON(data []byte) error {
+	var tmp Book
+	if err := bson.Unmarshal(data, &tmp); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (b *Book) MarshalBSON() ([]byte, error) {
+	return bson.Marshal(b)
 }
